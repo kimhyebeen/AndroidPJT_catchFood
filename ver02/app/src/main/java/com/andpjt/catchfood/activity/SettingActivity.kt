60aff155a3f6fc7566282546057583384b8a940d
kimhyebeen <kimhbin@naver.com>
2020-11-13 01:51:27 +0900

    [v2.0.2] 언어 리팩토링 (Java to Kotlin) - SettingActivity.kt 생성

--- ver02/app/src/main/java/com/andpjt/catchfood/activity/SettingActivity.kt
+++ ver02/app/src/main/java/com/andpjt/catchfood/activity/SettingActivity.kt
@@ -1,12 +1,16 @@
 package com.andpjt.catchfood.activity
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.andpjt.catchfood.BR
 import com.andpjt.catchfood.R
+import com.andpjt.catchfood.adapter.SettingMenuRecyclerAdapter
 import com.andpjt.catchfood.databinding.ActivitySetBinding
 import com.andpjt.catchfood.model.Food
 import com.andpjt.catchfood.viewmodel.FoodViewModel
@@ -15,6 +19,8 @@ import com.andpjt.catchfood.viewmodel.FoodViewModelFactory
 class SettingActivity: AppCompatActivity() {
     private lateinit var vm: FoodViewModel
     private lateinit var binding: ActivitySetBinding
+    private lateinit var editDialog: AlertDialog
+    private val settingAdapter = SettingMenuRecyclerAdapter()
 
     override fun onResume() {
         super.onResume()
@@ -25,6 +31,23 @@ class SettingActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_set)
 
+        setBinding()
+        setDialog()
+
+        binding.setRecyclerView.apply {
+            adapter = settingAdapter
+            layoutManager = LinearLayoutManager(applicationContext)
+            setHasFixedSize(true)
+        }
+
+        vm.getAll().observe(this, {
+            settingAdapter.setContents(it)
+        })
+
+        // TODO("setting의 edit dialog")
+    }
+
+    private fun setBinding() {
         vm = ViewModelProvider(this, FoodViewModelFactory(application))
                 .get(FoodViewModel::class.java)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_set)
@@ -33,8 +56,16 @@ class SettingActivity: AppCompatActivity() {
         binding.setVariable(BR.vm, vm)
     }
 
+    private fun setDialog() {
+        val dialogView = LayoutInflater.from(applicationContext).inflate(R.layout.dialog_edit, null, false)
+        val builder = AlertDialog.Builder(applicationContext)
+        builder.setView(dialogView)
+        editDialog = builder.create()
+    }
+
     fun clickAddButton(view: View) {
-        vm.insert(Food(null, vm.settingEditText.value ?: "", vm.settingRatingCount.value ?: 3))
+        vm.insert(Food(null, vm.settingEditText.value ?: "", vm.settingRatingCount.value?.toInt() ?: 3))
+        vm.initSetting()
     }
 
     fun clickCleanButton(view: View) {
